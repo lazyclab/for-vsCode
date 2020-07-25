0a3b10984ddcd62264e2a3a69432f949bf81435e
[Sarmen Bejanyan] <[sarmen.bejanyan@bk.ru]>
2020-07-25 21:17:04 +0400

    now

---
+++ js/mine.js
@@ -0,0 +1,59 @@
+
+//  Աշխատանք cosnole-ների հետ 
+/*console.log("Hello");
+console.warn("This is warning");
+console.error("This is error");*/
+
+// Աշխատանք թվերի հետ 
+//  var x = 15,y = 15;
+
+//  console.log(x+y);
+//  console.log("hello world "+y+ " this is a first nuber");
+
+// Աշխատանք փոփոխակաների և դինամիկ տեքստերի հետ 
+// let x = "worlds";
+// console.log("Hello " +x+" by Sarmen");
+
+// let myName = "Sarmen",mySety = "Ijevan",InfoAboutMe = `My name is ${myName} and i leave in ${mySety}`;
+// console.log(InfoAboutMe);
+
+
+// var z=15;
+// var a = [15,20,25,30];
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[2]);
+// var merTaxy ={
+//    harevan1:{
+//        name:"Artur",
+//        tariq:"45",
+//        andamneriQanak:"6",
+//    },
+//    harevan2:"Karen",
+//    harevan3:"Arman",
+// };
+// console.log(merTaxy.harevan1.name);
+// console.log(merTaxy.harevan1.tariq);
+// console.log(merTaxy.harevan1.andamneriQanak);
+
+// if(5 == 6){
+// console.log("ajs pajmany chist e");
+// }else if(5 == 4){
+//     console.log("ays mi tarberaky chist e");
+// } 
+// else{
+//     console.log("sxal arjeq");
+// }
+
+// function myFirstFunction(z){
+//     for(let g=0;g<=z;g++){
+//         console.log(g);
+//     }
+// }
+
+// myFirstFunction(1);
+// myFirstFunction(2);
+// myFirstFunction(3);
+// myFirstFunction(4);
+
+console.log('');
